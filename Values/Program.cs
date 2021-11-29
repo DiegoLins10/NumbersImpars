a899631b50f9a580b97051dde07dc53e4c349d39
DiegoLins10 <diegofernandeslins@gmail.com>
2021-11-28 23:07:37 -0300

    usando func lambdas

--- Values/Program.cs
+++ Values/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Values
 {
@@ -9,7 +10,15 @@ namespace Values
         {
             int[] vect = new int[] { 3, 4, 5 };
             ChangeOddValues(vect);
+            //var a = vect.TakeWhile(n => n < 3);
             Console.WriteLine(string.Join(" ", vect));
+            //Console.WriteLine(string.Join(" ", a));
+
+            int[] numbers = { 5, 4, 9, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var firstSmallNumbers = numbers.TakeWhile((n, index) => n >= index); //pega os valores no array enquanto o numero for maior que o index
+            Console.WriteLine(string.Join(" ", firstSmallNumbers));
+            // Output:
+            // 5 4
         }
         public static void ChangeOddValues(int[] numbers)
         {
